8e522bfad666bfc76925693fba02a0a43846409d
guilacerda <guiglacerda@gmail.com>
2018-09-21 16:37:18 -0300

    obter barra vermelha por novo teste para a mesma unidade previamente implementada - Rendimento bruto

--- src/Tests/RendimentoBrutoTestes.java
+++ src/Tests/RendimentoBrutoTestes.java
@@ -14,4 +14,10 @@ public class RendimentoBrutoTestes {
         assertEquals(13.97f, rentabilidade.rendimentoBruto(), 0f);
     }
 
+    @Test
+    public void gerarNovoRendimentoBrutoTest(){
+        Rentabilidade rentabilidade = new Rentabilidade();
+
+        assertEquals(13.15f, rentabilidade.rendimentoBruto(), 0f);
+    }
 }

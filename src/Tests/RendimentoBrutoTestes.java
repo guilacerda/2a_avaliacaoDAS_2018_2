e7c44577f978cd50ef989bb7c9d5fc167192007f
guilacerda <guiglacerda@gmail.com>
2018-09-21 16:59:13 -0300

    obter barra verde triangular e parametrizar - Rendimento Bruto

--- src/Tests/RendimentoBrutoTestes.java
+++ src/Tests/RendimentoBrutoTestes.java
@@ -1,23 +1,46 @@
 package Tests;
 
 import Calculos.Rentabilidade;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
 
 import static org.junit.Assert.assertEquals;
 
+@RunWith(Parameterized.class)
 public class RendimentoBrutoTestes {
+    Rentabilidade rentabilidade = new Rentabilidade();
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> Composto() {
+        return Arrays.asList(new Object[][]{
+                {0.1644f, 1000.0f, 0.085f, 13.97f},
+                {0.3288f, 500.0f, 0.08f, 13.15f},
+                {0.6576f, 3000.0f, 0.09f, 177.53f}
+        });
+    }
+
+    @Parameterized.Parameter()
+    public float tempo;
+
+    @Parameterized.Parameter(1)
+    public float aInicial;
+
+    @Parameterized.Parameter(2)
+    public float tJuros;
+
+    @Parameterized.Parameter(3)
+    public float expected;
+
+    static final float DELTA = 0.1f;
 
     @Test
     public void gerarRendimentoBrutoTest(){
-        Rentabilidade rentabilidade = new Rentabilidade();
-
-        assertEquals(13.97f, rentabilidade.rendimentoBruto(0.1644f, 1000.0f, 0.085f), 0.1f);
-    }
-
-    @Test
-    public void gerarNovoRendimentoBrutoTest(){
-        Rentabilidade rentabilidade = new Rentabilidade();
-
-        assertEquals(13.15f, rentabilidade.rendimentoBruto(0.3288f, 500.0f, 0.08f), 0.1f);
+        assertEquals(expected, rentabilidade.rendimentoBruto(tempo, aInicial, tJuros), DELTA);
     }
 }

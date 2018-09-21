1e83d9d4226f61da089c2ec706cea4a0c552dc63
guilacerda <guiglacerda@gmail.com>
2018-09-21 17:04:26 -0300

    Imposto Renda - Obter barra vermelha por falsificacao

--- src/Calculos/Rentabilidade.java
+++ src/Calculos/Rentabilidade.java
@@ -8,4 +8,8 @@ public class Rentabilidade {
     public float rendimentoBruto(float tempo, float aInicial, float tJuros){
         return tempo * aInicial * tJuros;
     }
+
+    public float calculoImpostoRenda(){
+        return 3.14f;
+    }
 }

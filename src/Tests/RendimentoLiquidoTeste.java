5641b8141431aed2bdf40545504b6b2df50cea5c
guilacerda <guiglacerda@gmail.com>
2018-09-21 17:26:28 -0300

    Rendimento Liquido - Barra vermelha por falta de implementacao

---
+++ src/Tests/RendimentoLiquidoTeste.java
@@ -0,0 +1,13 @@
+package Tests;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class RendimentoLiquidoTeste {
+    @Test
+    public void rendimentoLiquidoTeste(){
+        Rentabilidade rentabilidade = new Rentabilidade();
+
+        assertEquals(1.0829f, rentabilidade.rendimentoLiquido(), 0.1f);
+    }
+}

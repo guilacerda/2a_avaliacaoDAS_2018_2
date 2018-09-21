45675dfa57af80e5f1cef310d19a88f08b396d1f
guilacerda <guiglacerda@gmail.com>
2018-09-21 17:05:32 -0300

    Imposto Renda - obter barra vermelha por novo teste para a unidade implementada

--- src/Tests/ImpostoRendaTeste.java
+++ src/Tests/ImpostoRendaTeste.java
@@ -12,4 +12,10 @@ public class ImpostoRendaTeste {
         Rentabilidade rentabilidade = new Rentabilidade();
         assertEquals(3.14f, rentabilidade.calculoImpostoRenda(), 0.1f);
     }
+
+    @Test
+    public void gerarImpostoRendaTest2(){
+        Rentabilidade rentabilidade = new Rentabilidade();
+        assertEquals(2.98f, rentabilidade.calculoImpostoRenda(), 0.1f);
+    }
 }

1ceb7f6de81f72215d7b31f9490f12358dca8cbe
guilacerda <guiglacerda@gmail.com>
2018-09-21 17:37:53 -0300

    Rendimento Liquido - obter barra verde apos novo teste. Triangular e parametrizar

--- src/Tests/RendimentoLiquidoTeste.java
+++ src/Tests/RendimentoLiquidoTeste.java
@@ -2,24 +2,48 @@ package Tests;
 
 import Calculos.Rentabilidade;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
 import static org.junit.Assert.assertEquals;
 
+@RunWith(Parameterized.class)
 public class RendimentoLiquidoTeste {
-    @Test
-    public void rendimentoLiquidoTeste(){
-        Rentabilidade rentabilidade = new Rentabilidade();
-        float rendimentoBruto  = rentabilidade.rendimentoBruto(0.1644f, 1000.0f, 0.085f);
-        float impostoRenda = rentabilidade.calculoImpostoRenda(0.225f, rendimentoBruto);
+    Rentabilidade rentabilidade = new Rentabilidade();
 
-        assertEquals(1.0829f, rentabilidade.rendimentoLiquido(rendimentoBruto, impostoRenda, 1000.0f), 0.1f);
+    @Parameterized.Parameters
+    public static Collection<Object[]> Composto() {
+        return Arrays.asList(new Object[][]{
+                {0.1644f, 1000.0f, 0.085f, 0.225f, 1.0829f},
+                {0.3288f, 500.0f, 0.08f, 0.225f, 2.0384f},
+                {0.6576f, 3000.0f, 0.09f, 0.20f, 4.7342f}
+        });
     }
 
-    @Test
-    public void rendimentoLiquidoTeste2(){
-        Rentabilidade rentabilidade = new Rentabilidade();
-        float rendimentoBruto  = rentabilidade.rendimentoBruto(0.3288f, 500.0f, 0.08f);
-        float impostoRenda = rentabilidade.calculoImpostoRenda(0.225f, rendimentoBruto);
+    @Parameterized.Parameter()
+    public float tempo;
 
-        assertEquals(2.0348f, rentabilidade.rendimentoLiquido(rendimentoBruto, impostoRenda, 500.0f), 0.1f);
+    @Parameterized.Parameter(1)
+    public float aInicial;
+
+    @Parameterized.Parameter(2)
+    public float tJuros;
+
+    @Parameterized.Parameter(3)
+    public float aliquota;
+
+    @Parameterized.Parameter(4)
+    public float expected;
+
+    static final float DELTA = 0.1f;
+    @Test
+    public void rendimentoLiquidoTeste(){
+        float rendimentoBruto  = rentabilidade.rendimentoBruto(tempo, aInicial, tJuros);
+        float impostoRenda = rentabilidade.calculoImpostoRenda(aliquota, rendimentoBruto);
+
+        assertEquals(expected, rentabilidade.rendimentoLiquido(rendimentoBruto, impostoRenda, aInicial), DELTA);
     }
 }
